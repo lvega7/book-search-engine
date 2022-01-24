749130ea699c585865323eedd0651afc550d5252
Lissette <lisssettemvega@gmail.com>
2022-01-23 21:06:21 -0500

    update schema and readme

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -2,11 +2,19 @@ const { User, Thought} = require('../models')
 
 const resolvers = {
     Query: {
-        me: async () => {
-          return Thought.find().sort({ createdAt: -1 });
-        }
-      }
-
+        me: async (parent, args, context) => {
+          if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+              .select('-__v -password')
+              .populate('thoughts')
+              .populate('friends');
+    
+            return userData;
+          }
+    
+          throw new AuthenticationError('Not logged in');
+        },
+    }
   };
   
   module.exports = resolvers;
