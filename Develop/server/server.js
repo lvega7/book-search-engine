f900a85664faab8fef90d80cbad93dc07e42a4c1
Lissette <lisssettemvega@gmail.com>
2022-01-25 12:34:01 -0500

    deploying

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -43,7 +43,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
